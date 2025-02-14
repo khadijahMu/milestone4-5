b4a28e4925502ad9dc3c697562c6b922082f8cd3
khadijah <khadeejahmuazu@gmail.com>
2025-02-15 00:32:43 +0100

    route fix

--- routes/flowerRoutes.js
+++ routes/flowerRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const upload = require("../config/multerConfig"); 
 const { addFlower, getAllFlowers, deleteFlower } = require("../controllers/flowerController");
 const router = express.Router();
-//Route to get all flowers
+
+//  Get all flowers
 router.get("/", getAllFlowers);
-//Route to add a new flower
+
+//  Add a new flower
 router.post("/", upload.single("image"), addFlower);
-//Route to delete a flower by ID
+
+// Delete a flower by ID
 router.delete("/:id", deleteFlower);
+
 module.exports = router;
